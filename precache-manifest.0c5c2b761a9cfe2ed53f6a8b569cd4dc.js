1e9eceefec0c8b502675d0b0cf64c56f9e35e277
Caleb Ventura <caleb.ventura.v@uni.pe>
2019-10-29 20:32:01 -0500

    Updates

---
+++ precache-manifest.0c5c2b761a9cfe2ed53f6a8b569cd4dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8773d60011bbb157c8d16fbd5ab072",
+    "url": "/Demipatiapp/index.html"
+  },
+  {
+    "revision": "cac4b7f96564bf07c46d",
+    "url": "/Demipatiapp/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "48539cbab6e3d83dab3e",
+    "url": "/Demipatiapp/static/css/main.99a6eba2.chunk.css"
+  },
+  {
+    "revision": "cac4b7f96564bf07c46d",
+    "url": "/Demipatiapp/static/js/2.fdd397bf.chunk.js"
+  },
+  {
+    "revision": "48539cbab6e3d83dab3e",
+    "url": "/Demipatiapp/static/js/main.cfd84312.chunk.js"
+  },
+  {
+    "revision": "816f953b38ba7cfcc89b",
+    "url": "/Demipatiapp/static/js/runtime-main.bd629e70.js"
+  }
+]);
