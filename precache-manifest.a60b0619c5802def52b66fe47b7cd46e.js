ac8af835e3210694257a8f361f0e27150fc1ad48
Caleb Ventura <caleb.ventura.v@uni.pe>
2019-10-30 15:53:37 -0500

    Updates

---
+++ precache-manifest.a60b0619c5802def52b66fe47b7cd46e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b051097d5f299b1824eaa28be5fb1623",
+    "url": "/demipatiapp/index.html"
+  },
+  {
+    "revision": "8f886c87892d4dd13fd9",
+    "url": "/demipatiapp/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "111fe06d8b22e6b711b3",
+    "url": "/demipatiapp/static/css/main.99a6eba2.chunk.css"
+  },
+  {
+    "revision": "8f886c87892d4dd13fd9",
+    "url": "/demipatiapp/static/js/2.7687c204.chunk.js"
+  },
+  {
+    "revision": "111fe06d8b22e6b711b3",
+    "url": "/demipatiapp/static/js/main.663eac4f.chunk.js"
+  },
+  {
+    "revision": "b5d4af7e3989b7459c6d",
+    "url": "/demipatiapp/static/js/runtime-main.cb4b60cc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Demipatiapp/precache-manifest.2b596cc28b44583c7dd143e3ba247962.js"
+  "/demipatiapp/precache-manifest.a60b0619c5802def52b66fe47b7cd46e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Demipatiapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/demipatiapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
