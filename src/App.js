aa5ca1b5237d6815aca0a76249e00ed9b9f3b499
Caleb Ventura <caleb.ventura.v@uni.pe>
2019-11-01 09:44:49 -0500

    nmee

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
       // Propiedades ojooooo IMPORTANTE
 
       <div className="App">
-        <Navegacion nombre=" Javier "/>
+        <Navegacion nombre="Javier"/>
         <Carousel/>
         {/*<Tareas/>*/}
 
